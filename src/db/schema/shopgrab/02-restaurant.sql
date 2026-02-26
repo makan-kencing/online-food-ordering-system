b314a755cecacbc88c87416b531774e2f4813369
makan-kencing <dizzynight2096@gmail.com>
2026-02-26 14:22:41 +0800

    feat: add opening_hour, closing_hour, and is_temporarily_closed to restaurant

--- src/db/schema/shopgrab/02-restaurant.sql
+++ src/db/schema/shopgrab/02-restaurant.sql
@@ -1,10 +1,13 @@
 CREATE TABLE restaurant
 (
-    id                INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    code              VARCHAR(10) UNIQUE                  NOT NULL,
-    name              VARCHAR(50)                         NOT NULL,
-    description       VARCHAR(200)                        NOT NULL,
-    introduction_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    image_url         VARCHAR(2083),
-    address_id        INT REFERENCES address (id)         NOT NULL
+    id                    INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    code                  VARCHAR(10) UNIQUE                  NOT NULL,
+    name                  VARCHAR(50)                         NOT NULL,
+    description           VARCHAR(200)                        NOT NULL,
+    introduction_date     TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    image_url             VARCHAR(2083),
+    opening_hour          INTERVAL DAY(0) TO SECOND(0)        NOT NULL,
+    closing_hour          INTERVAL DAY(0) TO SECOND(0)        NOT NULL,
+    is_temporarily_closed BOOLEAN   DEFAULT FALSE             NOT NULL,
+    address_id            INT REFERENCES address (id)         NOT NULL
 );
