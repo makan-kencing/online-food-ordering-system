ad6a54de2e199240d82ab6d4513b606133998ac2
makan-kencing <dizzynight2096@gmail.com>
2026-03-06 00:57:10 +0800

    feat: split out order item and features

--- src/db/schema/shopgrab/03-order_item.sql
+++ src/db/schema/shopgrab/03-order_item.sql
@@ -1,12 +1,9 @@
 CREATE TABLE order_item
 (
-    id                    INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    order_id              INT REFERENCES orders (id)      NOT NULL,
-    product_id            INT REFERENCES product (id)      NOT NULL,
-    product_feature_id    INT REFERENCES product_feature (id),
-    feature_order_item_id INT REFERENCES order_item (id), -- used to denote the order line item the feature is applied to
-    quantity              INT CHECK ( quantity > 0 )       NOT NULL,
-    unit_price            DECIMAL CHECK ( unit_price > 0 ) NOT NULL,
-    remarks               VARCHAR(50),
-    CHECK ( product_feature_id IS NULL = feature_order_item_id IS NULL )
+    id         INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    order_id   INT REFERENCES orders (id)       NOT NULL,
+    product_id INT REFERENCES product (id)      NOT NULL,
+    quantity   INT CHECK ( quantity > 0 )       NOT NULL,
+    unit_price DECIMAL CHECK ( unit_price > 0 ) NOT NULL,
+    remarks    VARCHAR(50)
 );
---
+++ src/db/schema/shopgrab/04-order_item_feature.sql
@@ -0,0 +1,9 @@
+CREATE TABLE order_item_feature
+(
+    product_feature_id INT REFERENCES product_feature (id),
+    order_item_id      INT REFERENCES order_item (id), -- used to denote the order line item the feature is applied to
+    quantity           INT CHECK ( quantity > 0 )       NOT NULL,
+    unit_price         DECIMAL CHECK ( unit_price > 0 ) NOT NULL,
+    remarks            VARCHAR(50),
+    PRIMARY KEY (product_feature_id, order_item_id)
+);
