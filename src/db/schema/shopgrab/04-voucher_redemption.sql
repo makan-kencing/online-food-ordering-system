accb6aded035fe775aba8266844dd01f2ab2913a
makan-kencing <dizzynight2096@gmail.com>
2026-03-12 15:03:17 +0800

    feat: add time range validation for redemption + optimize query

--- src/db/schema/shopgrab/04-voucher_redemption.sql
+++ src/db/schema/shopgrab/04-voucher_redemption.sql
@@ -11,7 +11,7 @@ create trigger t_check_voucher_is_available_to_redeem
     on voucher_redemption
     for each row
 declare
-    v_voucher_id int;
+    v_voucher_id         int;
     v_within_usage_limit boolean;
     v_within_redeem_time boolean;
     voucher_fully_redeemed exception;
@@ -19,14 +19,16 @@ declare
     pragma exception_init ( voucher_fully_redeemed, -4096 );
 begin
     begin
-        select v.id, count(vvr.voucher_distribution_id) < v.usage_limit, i.invoiced_at between v.from_date and coalesce(v.thru_date, current_timestamp)
+        select v.id,
+               count(vvr.voucher_distribution_id) < v.usage_limit,
+               i.invoiced_at between v.from_date and coalesce(v.thru_date, current_timestamp)
         into v_voucher_id, v_within_usage_limit, v_within_redeem_time
-            from voucher_distribution vd
-            join invoice i on i.id = new.invoice_id
-            join voucher v  on vd.voucher_id = v.id
-            left join voucher_distribution vvd on vvd.voucher_id = v.id
-            left join voucher_redemption vvr on vvr.voucher_distribution_id = vvd.id
-        where vd.id = new.voucher_distribution_id
+        from voucher_distribution vd
+                 join invoice i on i.id = :new.invoice_id
+                 join voucher v on vd.voucher_id = v.id
+                 left join voucher_distribution vvd on vvd.voucher_id = v.id
+                 left join voucher_redemption vvr on vvr.voucher_distribution_id = vvd.id
+        where vd.id = :new.voucher_distribution_id
         group by v.id, v.usage_limit, v.from_date, v.thru_date, i.invoiced_at;
     exception
         when no_data_found then
@@ -54,17 +56,17 @@ create trigger t_check_voucher_can_change_usage_limit
     on voucher
     for each row
 declare
-    used int;
+    v_number_of_redeems boolean;
     voucher_insufficient exception;
     pragma exception_init ( voucher_insufficient, -4096 );
 begin
     select count(*)
-    into used
+    into v_number_of_redeems
     from voucher_redemption vr
-             join voucher_distribution vd on vr.voucher_distribution_id = vd.id
-    where vd.voucher_id = new.id;
+        join voucher_distribution vd on vd.id = vr.voucher_distribution_id
+    where vd.voucher_id = :new.id;
 
-    if (used >= new.usage_limit) then
+    if (v_number_of_redeems >= :new.usage_limit) then
         raise voucher_insufficient;
     end if;
 exception
@@ -72,6 +74,6 @@ exception
         raise_application_error(
                 -20300,
                 'Cannot set limit from ' || old.usage_limit || ' to ' || new.usage_limit || ' ' ||
-                'when ' || used || ' is in use.'
+                'when ' || v_number_of_redeems || ' is in use.'
         );
 end;
