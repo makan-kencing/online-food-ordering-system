10d011f86753285fd6991a3409e7a335f1e45141
makan-kencing <dizzynight2096@gmail.com>
2026-03-12 14:56:21 +0800

    feat: add time range validation for redemption

--- src/db/schema/shopgrab/04-voucher_redemption.sql
+++ src/db/schema/shopgrab/04-voucher_redemption.sql
@@ -9,40 +9,42 @@ create trigger t_check_voucher_is_available_to_redeem
     before
         insert
     on voucher_redemption
+    for each row
 declare
-    redeemed_voucher_id int;
-    used                int;
-    available           int;
+    v_voucher_id int;
+    v_within_usage_limit boolean;
+    v_within_redeem_time boolean;
     voucher_fully_redeemed exception;
+    voucher_redeem_out_of_time exception;
     pragma exception_init ( voucher_fully_redeemed, -4096 );
 begin
     begin
-        select v.id, v.usage_limit
-        into redeemed_voucher_id, available
-        from voucher_distribution vd
-                 join voucher v on v.id = vd.voucher_id
-        where vd.id = new.voucher_distribution_id;
+        select v.id, count(vvr.voucher_distribution_id) < v.usage_limit, i.invoiced_at between v.from_date and coalesce(v.thru_date, current_timestamp)
+        into v_voucher_id, v_within_usage_limit, v_within_redeem_time
+            from voucher_distribution vd
+            join invoice i on i.id = new.invoice_id
+            join voucher v  on vd.voucher_id = v.id
+            left join voucher_distribution vvd on vvd.voucher_id = v.id
+            left join voucher_redemption vvr on vvr.voucher_distribution_id = vvd.id
+        where vd.id = new.voucher_distribution_id
+        group by v.id, v.usage_limit, v.from_date, v.thru_date, i.invoiced_at;
     exception
         when no_data_found then
             raise_application_error(-20322, 'Voucher cannot be found');
     end;
 
-    if available is null then
-        return;
+    if (v_within_usage_limit = true) then
+        raise voucher_fully_redeemed;
     end if;
 
-    select count(*)
-    into used
-    from voucher_redemption vr
-             join voucher_distribution vd on vr.voucher_distribution_id = vd.id
-    where vd.voucher_id = redeemed_voucher_id;
-
-    if (used >= available) then
-        raise voucher_fully_redeemed;
+    if (v_within_redeem_time = false) then
+        raise voucher_redeem_out_of_time;
     end if;
 exception
     when voucher_fully_redeemed then
-        raise_application_error(-20300, 'Voucher could not be redeemed');
+        raise_application_error(-20300, 'Voucher fully redeemed');
+    when voucher_redeem_out_of_time then
+        raise_application_error(-20300, 'Voucher cannot be redeemed out of the available time range');
 end;
 
 
@@ -50,6 +52,7 @@ create trigger t_check_voucher_can_change_usage_limit
     before
         update of usage_limit
     on voucher
+    for each row
 declare
     used int;
     voucher_insufficient exception;
