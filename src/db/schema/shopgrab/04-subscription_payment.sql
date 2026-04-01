955bc557295fb5451f2e934b17dc191d223da0ee
makan-kencing <dizzynight2096@gmail.com>
2026-04-02 00:02:37 +0800

    feat: make monthly_subscription and payment many to many

---
+++ src/db/schema/shopgrab/04-subscription_payment.sql
@@ -0,0 +1,6 @@
+create table subscription_payment
+(
+    monthly_subscription_id INT REFERENCES monthly_subscription (id),
+    payment_id              INT REFERENCES payment (id),
+    PRIMARY KEY (monthly_subscription_id, payment_id)
+);
