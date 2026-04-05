ac092df3145c2267b64923c2274efe42a77f07a6
tanyj0612 <tanyijia9558@gmail.com>
2026-04-05 23:54:40 +0800

    Feat : add status and enum domain

--- src/db/schema/shopgrab/04-subscription_payment.sql
+++ src/db/schema/shopgrab/04-subscription_payment.sql
@@ -1,7 +1,13 @@
+CREATE DOMAIN status AS ENUM
+(
+    ACTIVE,
+    UPGRADED
+);
+
 create table subscription_payment
 (
     monthly_subscription_id INT REFERENCES monthly_subscription (id),
     payment_id              INT REFERENCES payment (id),
-    status                  VARCHAR(20),
+    status                 status not null ,
     PRIMARY KEY (monthly_subscription_id, payment_id)
 );
