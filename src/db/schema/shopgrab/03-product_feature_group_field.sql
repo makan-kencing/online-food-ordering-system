2c46c398cd6efdc3a76483000767f2851fef980a
whoiscifer <leebincent@gmail.com>
2026-04-06 09:29:34 +0800

    Added index for feature id searching

--- src/db/schema/shopgrab/03-product_feature_group_field.sql
+++ src/db/schema/shopgrab/03-product_feature_group_field.sql
@@ -4,3 +4,5 @@ CREATE TABLE product_feature_group_field
     product_feature_id       INT REFERENCES product_feature (id),
     PRIMARY KEY (product_feature_group_id, product_feature_id)
 );
+
+CREATE INDEX idx_feature_group_field_feature ON product_feature_group_field(product_feature_id);
