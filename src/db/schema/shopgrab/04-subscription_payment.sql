683ccc67a9952a00a4fb7dc18171a74107c53801
tanyj0612 <tanyijia9558@gmail.com>
2026-04-07 02:06:31 +0800

    remove : remove the value will auto insert in to the subscription payment

--- src/db/schema/shopgrab/04-subscription_payment.sql
+++ src/db/schema/shopgrab/04-subscription_payment.sql
@@ -1,13 +1,8 @@
-CREATE DOMAIN status AS ENUM
-(
-    ACTIVE,
-    UPGRADED
-);
+
 
 create table subscription_payment
 (
     monthly_subscription_id INT REFERENCES monthly_subscription (id),
     payment_id              INT REFERENCES payment (id),
-    status                 status not null ,
     PRIMARY KEY (monthly_subscription_id, payment_id)
 );
