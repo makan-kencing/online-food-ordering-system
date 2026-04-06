00739e0e102bff8923353fffc045b726fda78ffd
makan-kencing <dizzynight2096@gmail.com>
2026-04-07 02:58:12 +0800

    feat: remove subscription_payment.status

--- src/db/schema/shopgrab/04-subscription_payment.sql
+++ src/db/schema/shopgrab/04-subscription_payment.sql
@@ -2,6 +2,5 @@ create table subscription_payment
 (
     monthly_subscription_id INT REFERENCES monthly_subscription (id),
     payment_id              INT REFERENCES payment (id),
-    status                 status not null ,
     PRIMARY KEY (monthly_subscription_id, payment_id)
 );
