21e4c7771e38c7166c499ae3a38a731f081fd50e
makan-kencing <dizzynight2096@gmail.com>
2026-04-07 14:45:15 +0800

    fix: add missing trailing /

--- src/db/schema/shopgrab/00-utils.sql
+++ src/db/schema/shopgrab/00-utils.sql
@@ -2,8 +2,10 @@ create or replace function CPAD(text IN VARCHAR, n IN INT, pad IN VARCHAR) retur
 begin
     return LPAD(RPAD(text, LENGTH(text) + (n - LENGTH(text)) / 2, pad), n, pad);
 end;
+/
 
 create or replace function REPEAT(text IN VARCHAR, n IN INT) return varchar as
 begin
     return RPAD(text, n, text);
 end;
+/
