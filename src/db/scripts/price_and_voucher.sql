bd13297689a4aaa25ca3249e3f5cdb2e2bd8a340
makan-kencing <dizzynight2096@gmail.com>
2026-04-08 15:01:20 +0800

    style: formatting

--- src/db/scripts/price_and_voucher.sql
+++ src/db/scripts/price_and_voucher.sql
@@ -23,22 +23,26 @@ SELECT P.ID,
        P.CODE,
        P.FROM_DATE,
        P.THRU_DATE,
-       P.PRICE_TYPE "PRICE_TYPE_ID",
-       DOMAIN_DISPLAY(P.PRICE_TYPE) "PRICE_TYPE_NAME",
+       P.PRICE_TYPE                                                               "PRICE_TYPE_ID",
+       DOMAIN_DISPLAY(P.PRICE_TYPE)                                               "PRICE_TYPE_NAME",
        P.DESCRIPTION,
-       CASE WHEN CURRENT_TIMESTAMP < P.FROM_DATE THEN 'Not active'
-            WHEN CURRENT_TIMESTAMP > P.THRU_DATE THEN 'Expired'
-            ELSE CAST(P.THRU_DATE - CURRENT_TIMESTAMP AS INTERVAL DAY(2) TO SECOND(3)) || ' days left'
-       END "STATUS",
+       CASE
+           WHEN CURRENT_TIMESTAMP < P.FROM_DATE THEN 'Not active'
+           WHEN CURRENT_TIMESTAMP > P.THRU_DATE THEN 'Expired'
+           ELSE CAST(P.THRU_DATE - CURRENT_TIMESTAMP AS INTERVAL DAY(2) TO SECOND(3)) || ' days left'
+           END                                                                    "STATUS",
        TRIM(LEADING ',' FROM DECODE(P.PRODUCT_ID, NULL, '', ', Product: ' || P.PRODUCT_ID)
            || DECODE(P.PRODUCT_FEATURE_ID, NULL, '', ', Product Feature: ' || P.PRODUCT_FEATURE_ID)
-           || DECODE(P.QUANTITY_BREAK_ID, NULL, '', ', Quantity: More than ' || QB.FROM_QUANTITY || DECODE(QB.THRU_QUANTITY, NULL, '', ' to ' || QB.THRU_QUANTITY))
-           || DECODE(P.ORDER_VALUE_ID, NULL, '', ', Order Value: More than ' || OV.FROM_AMOUNT || DECODE(OV.THRU_AMOUNT, NULL, '', ' to ' || OV.THRU_AMOUNT))
+           || DECODE(P.QUANTITY_BREAK_ID, NULL, '', ', Quantity: More than ' || QB.FROM_QUANTITY ||
+                                                    DECODE(QB.THRU_QUANTITY, NULL, '', ' to ' || QB.THRU_QUANTITY))
+           || DECODE(P.ORDER_VALUE_ID, NULL, '', ', Order Value: More than ' || OV.FROM_AMOUNT ||
+                                                 DECODE(OV.THRU_AMOUNT, NULL, '', ' to ' || OV.THRU_AMOUNT))
            || DECODE(P.RESTAURANT_ID, NULL, '', ', Restaurant: ' || P.RESTAURANT_ID)
            || DECODE(P.MEMBERSHIP_ID, NULL, '', ', Membership: ' || P.MEMBERSHIP_ID)
            || DECODE(P.VOUCHER_ID, NULL, '', ', Voucher: ' || P.VOUCHER_ID)
-           || DECODE(P.VENDOR_ID, NULL, '', ', Vendor: ' || P.VENDOR_ID)) "SCOPE",
-       TRIM(LEADING ' ' FROM DECODE(P.PERCENTAGE, NULL, TO_CHAR(P.AMOUNT, '$999,999.99'), TO_CHAR(P.PERCENTAGE * 100, '99.99') || '%')) "AMOUNT",
+           || DECODE(P.VENDOR_ID, NULL, '', ', Vendor: ' || P.VENDOR_ID))         "SCOPE",
+       TRIM(LEADING ' ' FROM DECODE(P.PERCENTAGE, NULL, TO_CHAR(P.AMOUNT, '$999,999.99'),
+                                    TO_CHAR(P.PERCENTAGE * 100, '99.99') || '%')) "AMOUNT",
        P.CREATED_BY_ID
 FROM PRICE_COMPONENT P
          LEFT JOIN QUANTITY_BREAK QB ON P.QUANTITY_BREAK_ID = QB.ID
